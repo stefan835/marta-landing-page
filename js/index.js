755f8df53de5e13807ea441f337203d1cc73caf8
Piotr Stefanowicz <stefan8357@gmail.com>
2017-12-03 23:05:00 +0100

    fixing navigation errors

--- js/index.js
+++ js/index.js
@@ -1,21 +1,42 @@
-$("#contact--btn").click(function () {
-  $('html, body').animate({
-    scrollTop: $("#contact").offset().top
-  }, 1000);
-});
-$("#offer-link").click(function () {
-  $('html, body').animate({
-    scrollTop: $("#offer").offset().top
-  }, 1000);
-});
-$("#portfolio-link").click(function () {
-  $('html, body').animate({
-    scrollTop: $("#portfolio").offset().top
-  }, 1000);
-});
-$("#contact-link").click(function () {
-  $('html, body').animate({
-    scrollTop: $("#contact").offset().top
-  }, 1000);
-});
-window.innerWidth > 768 ? $(".main").onepage_scroll() : null;
+window.innerWidth >= 768 ? desktopViewPrepare() : mobileViewPrepare();
+
+function mobileViewPrepare() {
+  $('.reload--info').remove();
+  $('.body-overflow').remove();
+  $("#contact--btn").click(function () {
+    $('html, body').animate({
+      scrollTop: $("#contact").offset().top
+    }, 1000);
+  });
+  $("#offer-link").click(function () {
+    $('html, body').animate({
+      scrollTop: $("#offer").offset().top
+    }, 1000);
+  });
+  $("#portfolio-link").click(function () {
+    $('html, body').animate({
+      scrollTop: $("#portfolio").offset().top
+    }, 1000);
+  });
+  $("#contact-link").click(function () {
+    $('html, body').animate({
+      scrollTop: $("#contact").offset().top
+    }, 1000);
+  });
+}
+
+function desktopViewPrepare() {
+  $(".main").onepage_scroll();
+  $("#contact--btn").click(function () {
+    $(".main").moveTo(4)
+  });
+  $("#offer-link").click(function () {
+    $(".main").moveTo(2)
+  });
+  $("#portfolio-link").click(function () {
+    $(".main").moveTo(3)
+  });
+  $("#contact-link").click(function () {
+    $(".main").moveTo(4)
+  });
+}
