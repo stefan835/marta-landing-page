852de34395ace346ecd8c5159d0f4599890759ee
Piotr Stefanowicz <stefan8357@gmail.com>
2017-12-08 02:44:42 +0100

    removing validation from server side script, adding UTF-8 coding

--- form.php
+++ form.php
@@ -1,3 +1,9 @@
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
 <?php
 if(isset($_POST['email'])) {
 
@@ -8,8 +14,6 @@ if(isset($_POST['email'])) {
     function died($error) {
         // your error code can go here
         echo "Niestety formularz nie został poprawnie wypełniony: ";
-        echo $error."<br /><br />";
-        echo "Popraw błędy i spróbuj ponownie.<br /><br />";
         die();
     }
 
@@ -26,27 +30,6 @@ if(isset($_POST['email'])) {
     $telephone = $_POST['telephone']; // not required
     $comments = $_POST['comments']; // required
 
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'Pole zawierające adres email nie zostało poprawnie wypełnione.<br />';
-  }
-
-    $string_exp = "/^[A-Za-z .'-]+$/";
-
-  if(!preg_match($string_exp,$name)) {
-    $error_message .= 'Pole zawierające imię nie zostało poprawnie wypełnione.<br />';
-  }
-
-  if(strlen($comments) < 2) {
-    $error_message .= 'Pole zawierające wiadomość nie zostało poprawnie wypełnione.<br />';
-  }
-
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
-
     $email_message = "Treść formularza poniżej.\n\n";
 
     function clean_string($string) {
@@ -61,6 +44,7 @@ if(isset($_POST['email'])) {
 
 // create email headers
 $headers = 'From: '.$email_from."\r\n".
+"Content-type: text/html; charset=UTF-8" . "\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
@@ -100,3 +84,6 @@ $headers = 'From: '.$email_from."\r\n".
 
 }
 ?>
+
+</body>
+</html>
